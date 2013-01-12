3dfb72b85c3abf10e20812aa0d2cc6b3e8c168a9
Timo Schilling <timo@schilling.io>
2013-01-12 14:51:28 +0100

    update asset_patch

--- lib/v2a-deploy/asset_patch.rb
+++ lib/v2a-deploy/asset_patch.rb
@@ -2,7 +2,17 @@ Capistrano::Configuration.instance(:must_exist).load do
   namespace :deploy do
     namespace :assets do
       task :symlink do ; end
+
+      task :precompile, :roles => assets_role, :except => { :no_release => true } do
+        run <<-CMD.compact
+          cd -- #{latest_release.shellescape} &&
+          #{rake} RAILS_ENV=#{rails_env.shellescape} #{asset_env} assets:precompile
+        CMD
+      end
+
+      task :update_asset_mtimes do ; end
       task :clean_expired do ; end
+      task :rollback do ; end
     end
   end
 end
